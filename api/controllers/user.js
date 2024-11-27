ff255c87f86733a88cb96ed6758d088f35996bcc
jjCampeotanos <ecvljfgss123@gmail.com>
2024-11-27 14:21:42 -0300

    comecei o user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,9 +2,35 @@ import { db } from "../db.js";
 
 export const getUsers = (req,res) => {
     const q = "SELECT * FROM usuarios";
+
     db.query(q, (err, data) => {
         if(err) return res.json(err);
 
         return res.status(200).json(data);
     });
 };
+
+export const addUser = (req, res) => {
+    const q =
+      "INSERT INTO usuarios(`nome', `email`, `fone`, `data_nascimento`) VALUES (?)"
+
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.data_nascimento,
+    ];
+
+    db.query(q, [values], (err) => {
+        if (err) return res.jason(err);
+
+        return res.status(200).jason("Usuário criado com sucesso.");
+    });
+};
+
+export const updateUser = (req, res) => {
+    const q =
+      "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ?
+      WHERE `id` = ?";
+
+
+}
