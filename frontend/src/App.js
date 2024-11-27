362df408af71c50b27b28154e497e45083a03533
jjCampeotanos <ecvljfgss123@gmail.com>
2024-11-27 12:03:03 -0300

    conf app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import createGlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./components/Form.js";
 import Grid from "./components/Grid";
+import { userState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import axios from "axios";
 
 const Container = styled.div`
  width: 100%;
@@ -16,7 +17,20 @@ const Container = styled.div`
  gap: 10px;
 `;
 
+const Title = styled.h2 ``;
+
 function App() {
+    const [users, setUsers] =userState([]);
+    const [onEdit, setOnEdit] = userState(null);
+
+    const getUsers = async () => {
+        try {
+            const res = await axios.get("http://localhost:8800");
+            setUsers(res.data.sort((a,b) => (a.nome > b.nome ? 1 : -1 )));
+        } catch (error) {
+            toast.error(error);
+        }
+    };
 
     return (
         <>
