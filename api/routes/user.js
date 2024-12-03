c6bc75756608d0feefa25df3eb273fa42c97f951
jjCampeotanos <ecvljfgss123@gmail.com>
2024-12-03 08:36:20 -0300

    atualizar feito

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,11 +4,11 @@ import { Router } from "express";
 const rout_user = Router();
 
 rout_user.post("/user", (req, res) => {
-    const {nome, email, fone, data_nascimento}= req.body;
-    
-    db.query(`insert into usuarios (nome, email, fone, data_nascimento) values ('${nome}', '${email}','${fone}','${data_nascimento}')`,(err, result)=>{
-        if(err){
-            res.json("Erro ao inserir user: "+err.message)
+    const { nome, email, fone, data_nascimento } = req.body;
+
+    db.query(`insert into usuarios (nome, email, fone, data_nascimento) values ('${nome}', '${email}','${fone}','${data_nascimento}')`, (err, result) => {
+        if (err) {
+            res.json("Erro ao inserir user: " + err.message)
         }
 
         res.json(`Usuário ${nome} inserido com sucesso!`)
@@ -16,11 +16,10 @@ rout_user.post("/user", (req, res) => {
 })
 
 
-rout_user.get("/lista", (req, res) =>
-{
+rout_user.get("/lista", (req, res) => {
     db.query(`select * from usuarios`, (err, result) => {
-        if(err){
-            return res.json("erro ao listar"+err.message)
+        if (err) {
+            return res.json("erro ao listar" + err.message)
         }
 
         res.json(result);
@@ -29,29 +28,29 @@ rout_user.get("/lista", (req, res) =>
 
 rout_user.delete("/delete", (req, res) => {
     const { id } = req.body;
-    db.query(`delete from usuarios where id =${id}`),(err,result) =>{
-        if (err){
+    db.query(`delete from usuarios where id=${id}`), (err, result) => {
+        if (err) {
             return res.json(err.message)
         }
     }
 
     res.json({
-        Deletado:`produto${result.name} deletado com sucesso`
+        Deletado: `produto deletado com sucesso`
     });
-    });
-   
-rout_user.post ("/atualização", (req, res) => {
-  const { nome, email, fone, data_nascimento, id} = req.body;
+});
 
-  db.query(`update nomeTabela set cam poAnterior='novoCampo'
-    where id=${id}`,(err,result)=> {
-    
-    res.json ({
-    Edição: `Produto alterado com sucesso!`
-    })
-    })
-    })
+rout_user.put("/atualizacao", (req, res) => {
+    const { nome, email, fone, data_nascimento, id } = req.body;
 
-    
+    db.query(`update usuarios set nome='${nome}',update usuarios set email='${email}',update usuarios set fone='${fone}',update usuarios set data_nascimento='${data_nascimento}',
+    where id=${id}`, (err, result) => {
+
+        res.json({
+            Edição: `Produto alterado com sucesso!`
+        })
+    })
+})
+
+
 
 export { rout_user }
